9154a31baa16b65d7137c58b236c1ae99a451093
Shiqi <cuisq0418@gmail.com>
2023-11-29 19:55:41 +0800

    completed first part of lab1

---
+++ lab1/HelloNumbers.java
@@ -0,0 +1,16 @@
+
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code
+    public class HelloNumbers {
+        public static void main(String[] args) {
+            int x = 0;
+            while (x < 10) {
+                System.out.print(x + " ");
+                x = x + 1;
+            }
+        }
+    }
+
+
+
---
+++ lab1/HelloWorld.java
@@ -0,0 +1,14 @@
+import java.awt.*;
+import java.text.NumberFormat;
+import java.util.*;
+import java.util.List;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+
+
+}
