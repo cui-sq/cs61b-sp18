1512053c72cd14b331bb704bd11877f1b2175055
Shiqi <cuisq0418@gmail.com>
2023-12-17 15:41:30 +0800

    completed LeapYear of lab1

--- lab1/LeapYear.java
+++ lab1/LeapYear.java
@@ -29,5 +29,17 @@ public class LeapYear {
             }
         }
     }
+
+    /**
+     * @param year is a leap year or not */
+
+    public static boolean isLeapYear(int year){
+        if(year % 100 == 0){
+            if(year % 400 == 0) return true;
+            else return false;
+        }
+        else if(year % 4 == 0) return true;
+        else return false;
+    }
 }
 
