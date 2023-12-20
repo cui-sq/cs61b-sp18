73693e75352ceb1d73df2dc9e076da4194b17d75
Shiqi <cuisq0418@gmail.com>
2023-12-20 19:21:15 +0800

    Finished lab2

--- lab2/Intlist/IntList.java
+++ lab2/Intlist/IntList.java
@@ -82,7 +82,21 @@ public class IntList {
 
     public static IntList dcatenate(IntList A, IntList B) {
         //TODO:  fill in method
-        return null;
+        if (A == null) {
+            A = B;
+            return A;
+        }
+
+       /* Iterative Method
+       IntList ptr = A;
+       while (ptr.rest != null){
+            ptr = ptr.rest;
+        }
+       ptr.rest = B;
+       return A;*/
+        /* Recursive Method */
+        A.rest = dcatenate(A.rest, B);
+        return A;
     }
 
     /**
@@ -91,7 +105,26 @@ public class IntList {
      */
     public static IntList catenate(IntList A, IntList B) {
         //TODO:  fill in method
-        return null;
+        /* Recursive Method
+        if(A == null){
+            return B;
+        }
+
+        return new IntList(A.first, catenate(A.rest, B));
+        */
+        if(A == null){
+            return B;
+        }
+        IntList res = new IntList(A.first, null);
+        IntList ptr = res;
+        A = A.rest;
+        while(A != null){
+            ptr.rest = new IntList(A.first, null);
+            ptr = ptr.rest;
+            A = A.rest;
+        }
+        ptr.rest = B;
+        return res;
     }
 
 
