74a1775285418aa295a1f6782bb7118230d6200a
Shiqi <cuisq0418@gmail.com>
2023-12-28 16:23:14 +0800

    Fix the failure sequences by adding operations.

--- proj1gold/TestArrayDequeGold.java
+++ proj1gold/TestArrayDequeGold.java
@@ -7,7 +7,7 @@ public class TestArrayDequeGold {
         ArrayDequeSolution<Integer> ads = new ArrayDequeSolution<>();
         StringBuilder message = new StringBuilder();
         for (int i = 0; i < 500; i += 1) {
-            assertEquals(sad.isEmpty(), ads.isEmpty());
+            //assertEquals(sad.isEmpty(), ads.isEmpty());
             if (sad.isEmpty() && ads.isEmpty()) {
                 sad.addFirst(i);
                 ads.addFirst(i);
@@ -20,12 +20,17 @@ public class TestArrayDequeGold {
                 sad.addFirst(i);
                 ads.addFirst(i);
                 message.append("addFirst(").append(i).append(")\n");
+                message.append("get(0)\n");
                 assertEquals(message.toString(), ads.get(0), sad.get(0));
             } else if (numberBetweenZeroAndOne < 0.5) {
                 sad.addLast(i);
                 ads.addLast(i);
+
+                int posAds = ads.size() - 1;
                 message.append("addLast(").append(i).append(")\n");
-                assertEquals(message.toString(), ads.removeLast(), sad.removeLast());
+                message.append("size()\n");
+                message.append("get(").append(posAds).append(")\n");
+                assertEquals(message.toString(), ads.get(posAds), sad.get(posAds));
             } else if (numberBetweenZeroAndOne < 0.75) {
                 message.append("removeFirst()\n");
                 assertEquals(message.toString(), ads.removeFirst(), sad.removeFirst());
